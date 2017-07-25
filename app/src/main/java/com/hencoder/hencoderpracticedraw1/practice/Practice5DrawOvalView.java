c695076e2bb4d5bf1c7da9ca34ced330810aece7
huxiaowei <huxiaowei008@126.com>
2017-07-25 20:52:14 +0800

    练习到8

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice5DrawOvalView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice5DrawOvalView.java
@@ -2,6 +2,8 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -25,5 +27,12 @@ public class Practice5DrawOvalView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawOval() 方法画椭圆
+
+        Paint paint = new Paint();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            //left, top, right, bottom 是这个椭圆的左、上、右、下四个边界点的坐标
+            canvas.drawOval(50, 50, 150, 100, paint);
+        }
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
@@ -2,6 +2,8 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -25,5 +27,16 @@ public class Practice8DrawArcView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawArc() 方法画弧形和扇形
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        // left, top, right, bottom 描述的是这个弧形所在的椭圆；
+        // startAngle 是弧形的起始角度（x 轴的正向，即正右的方向，是 0 度的位置；
+        // 顺时针为正角度，逆时针为负角度），sweepAngle 是弧形划过的角度；
+        // useCenter 表示是否连接到圆心，如果不连接到圆心，就是弧形，如果连接到圆心，就是扇形。
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            canvas.drawArc(200, 100, 800, 500, -110, 100, true, paint); // 绘制扇形
+            canvas.drawArc(200, 100, 800, 500, 20, 140, false, paint); // 绘制弧形
+            paint.setStyle(Paint.Style.STROKE); // 画线模式
+            canvas.drawArc(200, 100, 800, 500, 180, 60, false, paint); // 绘制不封口的弧形
+        }
     }
 }
